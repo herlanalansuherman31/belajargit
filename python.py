dccf6433db40a1bc33acb2beca6eaa91990d2780
herlanalansuherman31 <herlanalansuherman06@gmail.com>
2025-10-28 19:38:30 +0700

    penambahan python

---
+++ python.py
@@ -0,0 +1,26 @@
+a = 1
+print ("nilai a =", a)
+
+a += 4
+print ("nilai a =", a)
+
+a -= 4
+print ("nilai a =", a)
+
+a *= 4
+print ("nilai a =", a)
+
+a **= 4
+print ("nilai a =", a)
+
+a /= 9
+print ("nilai a =", a)
+
+a //= 4
+print ("nilai a =", a)
+
+b = 2
+print ("nilai b =", b, "binary =", format(b,"08b"))
+
+b <<= 2
+print ("nilai b =", b, "binary =", format(b,"08b"))
